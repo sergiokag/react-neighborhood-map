54bea770428001c3bd11dbe8097baae3fc1ee62d
Sergio Kagiema <sergio_kagiema@live.com>
2018-06-22 21:02:52 +0300

    Finalize the basic structure of the project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// core
+import React from 'react'
+import { Route } from 'react-router-dom'
+
+// css
+import './App.css'
+
+export default class App extends React.Component { 
+  render() {
+    return( 
+      <div>Hello Map World</div>
+    )
+  }
+}
