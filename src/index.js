b7aec8af8d059b896669203187028782fe79e0e2
Sergio Kagiema <sergio_kagiema@live.com>
2018-07-11 01:57:26 +0300

    WIP: Working on the service workers

--- src/index.js
+++ src/index.js
@@ -3,7 +3,11 @@ import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
+import registerSW from './serviceWorker'
 
 ReactDOM.render(
   <BrowserRouter><App /></BrowserRouter>, 
-document.getElementById('root'))
+document.getElementById('root'));
+
+// registering service worker
+registerSW();
