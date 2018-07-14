5c73779e89861eb7b601c4abafc7460b7656c1fd
Serge <sergio_kagiema@live.com>
2018-07-14 12:15:20 +0300

    Remove unecessary constructor

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,10 +5,6 @@ import React from 'react'
 import Map from './Map'
 
 export default class Main extends React.Component {
-  
-  constructor(props) {
-    super(props)
-  }
 
   render() {
     return (
